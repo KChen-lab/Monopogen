310cf2d4212e613ac08e97ef84469ea89510b1d0
JinzhuangDou <jinzhuangdou198706@gmail.com>
2023-04-29 09:00:35 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,21 +5,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="scPopGene",
-    version="v0.0",
+    name="Monopogen",
+    version="v1.0.0",
     author="Jinzhuang Dou",
-    author_email="",
-    description="",
+    author_email="jdou1@mdanderson.org",
+    description="SNV calling from single cell sequencing",
     long_description=long_description,
     long_description_content_type="",
     url="",
-    #packages=['scPopGene'], #setuptools.find_packages(),
     install_requires=[
         l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPL v3 or later",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
